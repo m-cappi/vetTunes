eaf94ed0e93631f7439afddc301aa22cc6ad4734
Martin Cappi <mcappi91@gmail.com>
2021-11-06 12:21:25 -0300

    preparing userInfo
    component to display avatar and basic info

--- app/screens/Account/User.js
+++ app/screens/Account/User.js
@@ -4,12 +4,13 @@ import {Button} from 'react-native-elements';
 import Toast from 'react-native-easy-toast';
 import {FirebaseContext} from '../../firebase';
 import Loading from '../../components/Loading';
+import UserInfo from '../../components/Account/UserInfo';
 
 const User = () => {
   const [userInfo, setUserInfo] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [loadingInfo, setLoadingInfo] = useState('');
-  const [reloadUser, setReloadUser] = useState(false);
+  const [reloadUser, setReloadUser] = useState(null);
 
   const {firebase} = useContext(FirebaseContext);
 
@@ -20,12 +21,16 @@ const User = () => {
       const user = await firebase.auth.currentUser;
       setUserInfo(user);
     })();
-    setReloadUser(false);
+    //setReloadUser((current)=>!current);
   }, [reloadUser]);
 
   return (
     <View style={styles.viewUserInfo}>
-      {userInfo && <Text>User screen...</Text>}
+      {userInfo && (
+        <>
+          <UserInfo toastRef={toastRef} userInfo={userInfo} />
+        </>
+      )}
       <Button
         title="Log out"
         buttonStyle={styles.btnLogout}
