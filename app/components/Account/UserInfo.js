3e789494a7465c06bb7f84ca59456addbaa7838d
Martin Cappi <mcappi91@gmail.com>
2021-11-06 12:29:23 -0300

    userInfo display

--- app/components/Account/UserInfo.js
+++ app/components/Account/UserInfo.js
@@ -1,14 +1,49 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Avatar, Accessory} from 'react-native-elements';
 
-const UserInfo = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
-}
+const UserInfo = ({toastRef, userInfo: {photoURL, displayName, email}}) => {
+  const changeAvatar = () => {};
+  return (
+    <View style={styles.viewUserInfo}>
+      <Avatar
+        rounded
+        size="large"
+        showEditButton
+        containerStyle={styles.avatarContainer}
+        source={
+          photoURL
+            ? {uri: photoURL}
+            : require('../../../assets/img/avatar-default.jpg')
+        }>
+        <Accessory onPress={changeAvatar} size={25} />
+      </Avatar>
+      <View>
+        <Text style={styles.userName}>
+          {displayName ? displayName : 'Anonymous'}
+        </Text>
+        {email && <Text>{email}</Text>}
+      </View>
+    </View>
+  );
+};
 
-export default UserInfo
+export default UserInfo;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewUserInfo: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#f2f2f2',
+    paddingVertical: 30,
+  },
+  avatarContainer: {
+    marginRight: 20,
+    backgroundColor: '#f2f2f2',
+  },
+  userName: {
+    fontWeight: 'bold',
+    paddingBottom: 5,
+  },
+});
