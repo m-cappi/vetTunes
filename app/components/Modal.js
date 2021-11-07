cb1e0cd0de22bb1d8286126280ee52b75977d460
Martin Cappi <mcappi91@gmail.com>
2021-11-07 11:43:03 -0300

    Create Modal.js
    to display forms and other components that don't need a proper screen

---
+++ app/components/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Overlay} from 'react-native-elements';
+
+const Modal = ({isVisible, setIsVisible, children}) => {
+  const toggleModal = () => setIsVisible(actual => !actual);
+  return (
+    <Overlay
+      isVisible={isVisible}
+      fullScreen={false}
+      overlayStyle={styles.overlay}
+      backdropStyle={styles.backdrop}
+      onBackdropPress={toggleModal}>
+      {children}
+    </Overlay>
+  );
+};
+
+export default Modal;
+
+const styles = StyleSheet.create({
+  overlay: {backgroundColor: '#fff', height: 'auto', width: '90%'},
+  backdrop: {backgroundColor: 'rgba(0,0,0,0.5)'},
+});
