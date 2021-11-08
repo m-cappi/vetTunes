0f56662b20f8b3616cb2e7b39188a07fd102e780
Martin Cappi <mcappi91@gmail.com>
2021-11-08 18:30:32 -0300

    Update Album List Component
    adds content handler and fixed item display

--- app/components/Albums/ListAlbums.js
+++ app/components/Albums/ListAlbums.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -12,20 +12,35 @@ import {isEmpty} from 'lodash';
 import {useNavigation} from '@react-navigation/native';
 import colors from '../../styles/palette';
 
-const ListAlbums = ({albums, handleLoadMore, isLoading}) => {
+const ListAlbums = ({albums}) => {
+  const [displayedAlbums, setDisplayedAlbums] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    setDisplayedAlbums(albums.getNextBatch());
+  }, [albums]);
+
+  const handleLoadMore = () => {
+    if (!albums.endOfList) {
+      setisLoading(true);
+      setDisplayedAlbums([...displayedAlbums, ...albums.getNextBatch()]);
+      setisLoading(false);
+    }
+  };
+
   return (
     <View>
-      {!isEmpty(albums) ? (
+      {!isEmpty(displayedAlbums) ? (
         <FlatList
-          data={albums}
+          data={displayedAlbums}
           renderItem={album => (
             <AlbumItem album={album} navigation={navigation} />
           )}
           keyExtractor={(item, index) => index.toString()}
           onEndReached={handleLoadMore}
           onEndReachedThreshold={0.5}
-          ListFooterComponent={ListFooter}
+          ListFooterComponent={<ListFooter isLoading={isLoading} />}
         />
       ) : (
         <View style={styles.viewLoading}>
@@ -40,7 +55,13 @@ const ListAlbums = ({albums, handleLoadMore, isLoading}) => {
 export default ListAlbums;
 
 const AlbumItem = ({album, navigation}) => {
-  const {id, albumName, artist, image} = album;
+  const {
+    item: {id, albumName, artist, category, pricing},
+  } = album;
+  const image = album.item.smImg();
+  const artistName = artist.name;
+  const categoryName = category.term;
+  const price = pricing.label;
   const goAlbum = () => {
     navigation.navigate('album', {
       id,
@@ -56,15 +77,19 @@ const AlbumItem = ({album, navigation}) => {
             PlaceholderContent={<ActivityIndicator color={colors.dark1} />}
             source={
               !isEmpty(image)
-                ? {uri: image}
+                ? {uri: image.url}
                 : require('../../../assets/img/no-image.png')
             }
             style={styles.imgAlbum}
           />
         </View>
-        <View>
+        <View style={styles.viewInfo}>
           <Text style={styles.albumName}>{albumName}</Text>
-          <Text style={styles.artist}>{artist}</Text>
+          <Text style={styles.artist}>{artistName}</Text>
+          <View style={styles.viewDetails}>
+            <Text style={styles.details}>{categoryName}</Text>
+            <Text style={styles.details}>{price}</Text>
+          </View>
         </View>
       </View>
     </TouchableOpacity>
@@ -74,9 +99,13 @@ const AlbumItem = ({album, navigation}) => {
 const ListFooter = ({isLoading}) => {
   return (
     <>
-      {isLoading && (
+      {isLoading ? (
         <View style={styles.viewLoading}>
           <ActivityIndicator size="large" color={colors.dark1} />
+        </View>
+      ) : (
+        <View style={styles.endOfList}>
+          <Text>End of list.</Text>
         </View>
       )}
     </>
@@ -88,6 +117,18 @@ const styles = StyleSheet.create({
   viewAlbum: {flexDirection: 'row', margin: 10},
   viewAlbumImg: {marginRight: 15},
   imgAlbum: {width: 80, height: 80},
+  viewInfo: {width: '70%'},
   albumName: {fontWeight: 'bold'},
   artist: {paddingTop: 2, color: 'grey'},
+  viewDetails: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 'auto',
+  },
+  details: {paddingTop: 2, color: 'grey', fontSize: 12},
+  endOfList: {
+    marginTop: 10,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
 });
