5ceecef95e562442fc13af27de7af463cab857e4
Martin Cappi <mcappi91@gmail.com>
2021-11-08 18:20:20 -0300

    debugging TopAlbums screen

--- app/screens/TopAlbums/TopAlbums.js
+++ app/screens/TopAlbums/TopAlbums.js
@@ -1,27 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, ScrollView} from 'react-native';
+import ListAlbums from '../../components/Albums/ListAlbums';
 import Loading from '../../components/Loading';
 import getAlbums from '../../utils/itunes';
 
 const TopAlbums = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [albums, setAlbums] = useState([]);
-  const [pseudoPagination, setPseudoPagination] = useState({from: 0, to: 20});
+
   useEffect(() => {
-    setAlbums(getAlbums());
-    setIsLoading(false);
+    (async () => {
+      const top100 = await getAlbums();
+      setAlbums(top100);
+      setIsLoading(false);
+    })();
   }, []);
   return (
-    <ScrollView>
+    <View>
       {isLoading ? (
         <Loading
           isVisible={isLoading}
           text="We are getting the best albums for you"
         />
       ) : (
-        <Text>Top Albums Screen...</Text>
+        <ListAlbums albums={albums} />
       )}
-    </ScrollView>
+    </View>
   );
 };
 
