88519b819c8974839d5b4ee71f20386c448f54e2
Martin Cappi <mcappi91@gmail.com>
2021-11-08 23:17:16 -0300

    Favorite Album list item layout

---
+++ app/components/Favorites/FavoriteAlbum.js
@@ -0,0 +1,110 @@
+import React, {useState, useRef, useCallback, useContext} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import {Image, Icon} from 'react-native-elements';
+import {isEmpty} from 'lodash';
+import colors from '../../styles/palette';
+
+const FavoriteAlbum = ({
+  album,
+  navigation,
+  setIsLoading,
+  toastRef,
+  setReloadData,
+}) => {
+  const {
+    item: {id, albumName, artist, category, pricing},
+  } = album;
+  const image = album.item.mdImg();
+  const artistName = artist.name;
+  const categoryName = category.term;
+  const price = pricing.label;
+
+  const goAlbum = () => {
+    navigation.navigate('album', {
+      id,
+      albumName,
+    });
+  };
+
+  const confirmRemoveFavorite = () => {
+    Alert.alert(
+      'Remove Album from your Favorites',
+      'Are you certain about removing this album from your favorites?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          onPress: removeFavorite,
+        },
+      ],
+      {cancelable: false},
+    );
+  };
+
+  const removeFavorite = () => {
+    console.log('Album deleted!');
+  };
+
+  return (
+    <TouchableOpacity onPress={() => goAlbum()}>
+      <View style={styles.viewAlbum}>
+        <Icon
+          type="material-community"
+          name="heart"
+          color="#f00"
+          containerStyle={styles.favorite}
+          onPress={confirmRemoveFavorite}
+          underlayColor="transparent"
+        />
+        <View style={styles.viewAlbumImg}>
+          <Image
+            resizeMode="cover"
+            PlaceholderContent={<ActivityIndicator color={colors.dark1} />}
+            source={
+              !isEmpty(image)
+                ? {uri: image.url}
+                : require('../../../assets/img/no-image.png')
+            }
+            style={styles.imgAlbum}
+          />
+        </View>
+        <View style={styles.viewInfo}>
+          <Text style={styles.albumName}>{albumName}</Text>
+          <Text style={styles.artist}>{artistName}</Text>
+          <View style={styles.viewDetails}>
+            <Text style={styles.details}>{categoryName}</Text>
+            <Text style={styles.details}>{price}</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default FavoriteAlbum;
+
+const styles = StyleSheet.create({
+  viewAlbum: {flexDirection: 'row', margin: 10},
+  favorite: {position: 'absolute', top: 0, right: 0},
+  viewAlbumImg: {marginRight: 10},
+  imgAlbum: {width: 100, height: 100},
+  viewInfo: {width: '70%'},
+  albumName: {fontWeight: 'bold'},
+  artist: {paddingTop: 2, color: 'grey'},
+  viewDetails: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 'auto',
+  },
+  details: {paddingTop: 2, color: 'grey', fontSize: 12},
+});
