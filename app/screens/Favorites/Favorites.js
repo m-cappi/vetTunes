4bd69f5d603bea23f3afab42cd214a3f5d0d1e72
Martin Cappi <mcappi91@gmail.com>
2021-11-08 22:53:22 -0300

    Favorites base layout

--- app/screens/Favorites/Favorites.js
+++ app/screens/Favorites/Favorites.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import {View, Text} from 'react-native';
 
-const Favorites = () => {
+import {FirebaseContext} from '../../firebase';
+import colors from '../../styles/palette';
+import UserNotLogged from '../../components/Favorites/UserNotLogged';
+import NoFavorites from '../../components/Favorites/NoFavorites';
+
+const Favorites = ({navigation}) => {
+  const [isUserLogged, setIsUserLogged] = useState(null);
+  const [favoriteList, setFavoriteList] = useState(null);
+
+  const {firebase} = useContext(FirebaseContext);
+
+  firebase.auth.onAuthStateChanged(user => {
+    user ? setIsUserLogged(true) : setIsUserLogged(false);
+  });
+
+  if (!isUserLogged) return <UserNotLogged navigation={navigation} />;
+  if (!favoriteList) return <NoFavorites navigation={navigation} />;
   return (
     <View>
       <Text>Favorites Screen...</Text>
