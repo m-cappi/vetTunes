44a6cfdaa3c9ce046097ca326652cefaba60a301
Martin Cappi <mcappi91@gmail.com>
2021-11-09 16:55:54 -0300

    search stack detailed album

--- app/screens/Search/AlbumResult.js
+++ app/screens/Search/AlbumResult.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {View} from 'react-native';
 
-const AlbumResult = () => {
+import Loading from '../../components/Loading';
+import AlbumShowcase from '../../components/Albums/AlbumShowcase';
+import {ItunesContext} from '../../utils/itunes';
+
+const AlbumResult = ({navigation, route}) => {
+  const [album, setAlbum] = useState(null);
+  const {id, albumName} = route.params;
+
+  const top100 = useContext(ItunesContext);
+
+  useEffect(() => {
+    navigation.setOptions({title: albumName});
+    setAlbum(top100.findByPk(id));
+  }, [id]);
+
   return (
     <View>
-      <Text></Text>
+      <Loading isVisible={!album ? true : false} text="Loading..." />
+      {album && <AlbumShowcase album={album} />}
     </View>
   );
 };
 
 export default AlbumResult;
-
-const styles = StyleSheet.create({});
